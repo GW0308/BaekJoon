fd837e34a82c6edf6c9523d0a2e78144f2878fc1
maen3493 <30686331-maen3493@users.noreply.replit.com>
2023-12-24 06:31:05 +0000

    Baekjoon-2525

--- Main.java
+++ Main.java
@@ -2,34 +2,36 @@
 
 // import org.junit.jupiter.api.Test;
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int hours = sc.nextInt();
-    int minutes = sc.nextInt();
-    int times = sc.nextInt();
-    sc.close();
-    if(times>=60){
-        hours+=(times/60);
-        minutes+=(times%60);
+    while(true){
       
-        if(minutes>=60){
-            hours+=minutes/60;
-            minutes = minutes%60;
-        }
-    }
-    else{
-      if((minutes+=times)>=60){
-          hours += 1;
-          if(hours>=24){
-            hours -= 24;
-          }
-          System.out.print(minutes);
-          minutes = 60-minutes;
-      }
-    }
-    System.out.print(hours+" "+minutes);
-  }
+      int hours = sc.nextInt();
+      int minutes = sc.nextInt();
+      int times = sc.nextInt();
 
+      if (times >= 60) {
+        hours += times / 60;
+        minutes += times % 60;
+      }
+      else{
+        minutes+=times;
+      }
+      
+      if (minutes >= 60) {
+        hours += minutes/60;
+        minutes = minutes%60;
+      }
+      if (hours >= 24) {
+        hours -= 24;
+      }
+      
+
+      System.out.println(hours + " " + minutes);
+    }
+    
+  }
 
 }

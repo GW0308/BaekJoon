162400be9813e27935df525c5a2e22d4e0c74b7c
GW0308 <52944946+GW0308@users.noreply.github.com>
2023-12-28 10:52:17 +0000

    [Add]
    10811 problem complete.

--- baekjoon/step_6/No10811_2.java
+++ baekjoon/step_6/No10811_2.java
@@ -1,34 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class No10811 {
+public class No10811_2 {
     public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
+      try{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringTokenizer st = new StringTokenizer(br.readLine());
       
       int[] arr = new int[100];
-      int N = sc.nextInt();
-      int M = sc.nextInt();
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
       int temp;
       for (int i = 0; i < N; i++) {
         arr[i] = i+1;
       }
       for(int k = 0; k<M; k++){
-        
-        int i = sc.nextInt();
-        int j = sc.nextInt();
-        
+
+        int i = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(st.nextToken());
+
         for(; i<=j; i++){
           temp = arr[i-1];
           arr[i-1] = arr[j-1];
           arr[j-1] = temp;
           j--;
         }
-        
       }
-      
+      br.close();
+        
       for(int i = 0; i < N; i++){
         System.out.print(arr[i]+" ");
       }
-
-    }
+      }catch(Exception e){
+      
+      }
+    
+  }
 }

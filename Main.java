aaaa5bebde53a87e6266c07d72f0d330ca97e720
GW0308 <52944946+GW0308@users.noreply.github.com>
2023-12-31 06:59:23 +0000

    [add]
    No11718 problem complete.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
   public static void main(String args[]) {
     try{
-      No5622.main(args);
+      No11718.main(args);
     }catch(Exception e){}
     
   }
---
+++ baekjoon/step_7/No11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class No11718 {
+    public static void main(String[] args) throws IOException {
+      Scanner sc = new Scanner(System.in);
+
+      while (sc.hasNext()) {
+        System.out.println(sc.nextLine());
+      }
+
+    }
+}

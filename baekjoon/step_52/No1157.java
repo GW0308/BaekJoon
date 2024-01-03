60ce6c208124e7e7cda24240921d6363445a3329
GW0308 <52944946+GW0308@users.noreply.github.com>
2024-01-03 14:52:55 +0000

    [add][complete]
    No1157 problem finally complete.

--- baekjoon/step_52/No1157.java
+++ baekjoon/step_52/No1157.java
@@ -2,25 +2,39 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
 import java.io.IOException;
-import java.lang.StringBuilder;
+
 public class No1157 {
     public static void main(String[] args) throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
       int[] count = new int[26];
       char[] input = br.readLine().toUpperCase().toCharArray();
-      char max = ' ';
+      int max = -1;
+      char maxChr = ' ';
+      
       for (int i = 0; i < input.length; i++) {
-        count[(int)(input[i]-65)] += 1;
-        System.out.println(count[i]);
+        count[(int)(input[i])-65]++;
       }
-
-      for (int i = 0; i < count.length; i++) {
-        if(count[i]>count[i+1]){
-          max = (char)(i+65);        
+      for (int i = 0; i < 26; i++) {
+        if(count[i]>max){
+          max = count[i];      
+          maxChr = (char)(i+65);
         }
+        else if(count[i]==max){
+          maxChr = '?';
+        }
+          
       }
-      System.out.println(1234);
+   
+
+      if(input.length == 1) 
+        bw.write(String.valueOf(input[0]));
+      else
+        bw.write(String.valueOf(maxChr));
+     
+      bw.flush();
     }
 }

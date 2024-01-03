786864268e27d4723394e33638769843bd4ed237
GW0308 <52944946+GW0308@users.noreply.github.com>
2024-01-03 11:26:35 +0000

    [add]
    No1157 problem complete yet

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main{
   public static void main(String args[]) {
     try{
-      No10988.main(args);
+      No1157.main(args);
     }catch(Exception e){}
     
   }
--- baekjoon/step_52/No1157.java
+++ baekjoon/step_52/No1157.java
@@ -3,11 +3,24 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
-
+import java.lang.StringBuilder;
 public class No1157 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // Your code goes here
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      int[] count = new int[26];
+      char[] input = br.readLine().toUpperCase().toCharArray();
+      char max = ' ';
+      for (int i = 0; i < input.length; i++) {
+        count[(int)(input[i]-65)] += 1;
+        System.out.println(count[i]);
+      }
+
+      for (int i = 0; i < count.length; i++) {
+        if(count[i]>count[i+1]){
+          max = (char)(i+65);        
+        }
+      }
+      System.out.println(1234);
     }
 }
